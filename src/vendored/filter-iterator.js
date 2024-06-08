098d3c0d4633662e591f5a6d84e47fa78becbce3
Eric Cornelissen <ericornelissen@gmail.com>
2024-06-08 12:16:43 +0200

    Improve licensing and vendoring information
    This improves license information and info about vendored dependencies
    and splits the license of this project between the original author and
    me and recent contributors.
    
    Update each vendored dependency file with improved information, in
    particular about what version was vendored. Also add the verbatim
    license text of each vendored dependency both to the vendored file and
    this project's license information. For this project's license file the
    structure is based on Node.js' license file as of commit
    `6aa9047f960e2385dac807e0661a159324b00b0f`.

--- src/vendored/filter-iterator.js
+++ src/vendored/filter-iterator.js
@@ -9,8 +9,31 @@ function* filterIterator(xs, pred) {
 }
 
 // This code was taken from:
-// - https://www.npmjs.com/package/filter-iterator
-// - https://github.com/jb55/filter-iterator
-// which is available under the MIT license
-// It was vendored to avoid dependency on this package,
-// which does not declare its license in package.json
+// - https://www.npmjs.com/package/filter-iterator (v0.0.1)
+// - https://github.com/jb55/filter-iterator (4ec4175efeced5e55cc80b8223d017c2f66152fa)
+// which is available under the MIT license, full license text below.
+//
+// It was vendored to avoid dependency on this package, which does not declare
+// its license in package.json
+
+// The MIT License(MIT)
+//
+// Copyright(c) 2014 William Casarin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
--- src/vendored/transform-spread-iterable.js
+++ src/vendored/transform-spread-iterable.js
@@ -13,7 +13,31 @@ function * spread(source) {
 module.exports = spread;
 
 // This code was taken from:
-// - https://www.npmjs.com/package/transform-spread-iterable
-// - https://github.com/parro-it/transform-spread-iterable
-// which is available under the MIT license
-// It was vendored to avoid the (unnecessary) dependency on hughfdjackson's curry package
+// - https://www.npmjs.com/package/transform-spread-iterable (v1.4.1)
+// - https://github.com/parro-it/transform-spread-iterable (89bda9986ff8be9f61a38f37d3950ffb2ab5f76a)
+// which is available under the MIT license, full license text below.
+//
+// It was vendored to avoid the (unnecessary) dependency on hughfdjackson's
+// curry package.
+
+// MIT License
+//
+// Copyright (c) 2016 Andrea Parodi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
